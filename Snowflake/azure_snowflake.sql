22c3e5899b1916c96c5b39577b08bddb342b70df
VinithaVudhayagiri <115439655+VinithaVudhayagiri@users.noreply.github.com>
2024-09-02 18:59:19 -0400

    Update azure_snowflake.sql

--- Snowflake/azure_snowflake.sql
+++ Snowflake/azure_snowflake.sql
@@ -3,7 +3,7 @@ create storage integration azure_snowflake_integration
     storage_provider = azure
     azure_tenant_id = '5fd8c3fb-e858-4d84-956d-3c0bf74e7e98'
     enabled = true
-    storage_allowed_locations = ( 'azure://snowflakeazureintegrate.blob.core.windows.net/snowfalkeintegration/');
+    storage_allowed_locations = ( 'azure://<>.blob.core.windows.net/snowfalkeintegration/');
 
 DESC STORAGE INTEGRATION azure_snowflake_integration;
 
@@ -12,7 +12,7 @@ USE SCHEMA AZUREINTEGRATION;
 
 CREATE STAGE azure_stage
   STORAGE_INTEGRATION = azure_snowflake_integration
-  URL = 'azure://snowflakeazureintegrate.blob.core.windows.net/snowfalkeintegration/'
+  URL = 'azure://<>.blob.core.windows.net/snowfalkeintegration/'
   FILE_FORMAT = (type=json);
 
 CREATE OR REPLACE TABLE TEST_TABLE(
